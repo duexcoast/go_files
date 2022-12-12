c9439f0cf74e286a267fc566b44bf4cda7db5e8f
duexcoast <conor.ux@gmail.com>
2022-12-12 11:32:15 -0500

    reading and extracting title metadata from posts

--- go_files.go
+++ go_files.go
@@ -1,10 +1,45 @@
 package go_files
 
-import "testing/fstest"
+import (
+	"io"
+	"io/fs"
+)
 
 type Post struct {
+	Title string
 }
 
-func NewPostsFromFS(filesystem fstest.MapFS) []Post {
-	return []Post{{}, {}}
+func NewPostsFromFS(filesystem fs.FS) ([]Post, error) {
+	dir, err := fs.ReadDir(filesystem, ".")
+
+	if err != nil {
+		return nil, err
+	}
+	var posts []Post
+
+	for _, f := range dir {
+		post, err := getPost(filesystem, f)
+		if err != nil {
+			return nil, err // needs clarification: should we totally fail if one file fails, or just ignore?
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
+
+func getPost(filesystem fs.FS, f fs.DirEntry) (Post, error) {
+	postFile, err := filesystem.Open(f.Name())
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return Post{}, err
+	}
+
+	post := Post{Title: string(postData)[7:]}
+	return post, nil
+
 }
--- go_files_test.go
+++ go_files_test.go
@@ -1,6 +1,7 @@
 package go_files_test
 
 import (
+	"reflect"
 	"testing"
 	"testing/fstest"
 
@@ -9,13 +10,24 @@ import (
 
 func TestNewBlogPosts(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello-world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello-world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 
-	posts := go_files.NewPostsFromFS(fs)
+	posts, err := go_files.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+
+	got := posts[0]
+	want := go_files.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
